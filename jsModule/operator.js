475f76d9216dc5b4cd7a3e70a881ce466eaba05a
aman-scaler <mohd.aman@scaler.com>
2023-11-29 15:25:04 +0530

    update

---
+++ jsModule/operator.js
@@ -0,0 +1,61 @@
+// -,+,*,=,==,===
+
+// var a = 10;
+// var b = 20;
+// console.log(a+b);
+
+
+// var a = 10;
+// var b = "20";
+// console.log(a+b);
+
+// var a = 120;
+// var b = "I am a str";
+// console.log(a+b);
+
+// var a = 2;
+// var b = 1;
+// console.log(a-b);
+
+// var a = 2;
+// var b = "1";
+// console.log(a-b);
+
+// var a = 2;
+// var b = "I am a str";
+// console.log(a-b);
+// console.log(typeof(NaN))
+
+// var a = 2;
+// var b = "I am a str";
+// console.log(b*a);
+
+// var a = "I am a str";
+// var b = true;
+
+// console.log(a+b);
+
+// if(3 == '3'){ //it checks only values
+//     console.log("equal");
+// }else{
+//     console.log("not equal");
+// }
+
+// if(3 === '3'){ //it checks value as well as type 
+//     console.log("equal");
+// }else{
+//     console.log("not equal");
+// }
+
+// if(NaN == NaN){
+//     console.log("Equal")
+// }else{
+//     console.log("Not equal");
+// }
+
+var c = 30;
+var stringC = String(c);
+console.log(typeof(stringC));
+console.log(typeof(c));
+
+var num = Number(stringC);
